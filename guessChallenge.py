8a6bc7db45215b3d335f8e00aaa0131ea74ebcae
root <root@DESKTOP-AUPBR5T.localdomain>
2020-07-18 21:15:04 +1000

    Removed repititive if statements

--- guessChallenge.py
+++ guessChallenge.py
@@ -4,26 +4,17 @@ answer = random.randint(1, 15)
 print(answer)
 
 print("Put your number here between 1 and 15: ")
-i = float(input())
-
+i = 0
 while i != answer:
+    i = float(input())
     if i == 0:
         print("Bye Bye !!!")
         break
-    elif i < answer:
-        print("Please guess higher")
-        i = float(input())
-        if i == answer:
-            break
-        #     print("Finally!!!")
-        # else:
-        #     continue
-    elif i > answer:
-        print("Please guess lower")
-        i = float(input())
-        if i == answer:
-            break
-        # else:
-        #     continue
-if i == answer:
-    print("You guessed it. It was {}".format(answer))
+    elif i == answer:
+        print("You guessed it. It was {}".format(answer))
+        break
+    else:
+        if i < answer:
+            print("Please guess higher")
+        else:
+            print("Please guess lower")
