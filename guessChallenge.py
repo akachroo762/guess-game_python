4b48ba773d6bb4bf779ef08f6034d62e73bbe48f
root <root@DESKTOP-AUPBR5T.localdomain>
2020-07-18 21:27:51 +1000

    Added some comments to make it readable

--- guessChallenge.py
+++ guessChallenge.py
@@ -5,16 +5,20 @@ answer = random.randint(1, highest)
 print(answer)
 
 print("Put your number here between 1 and {}: ".format(highest))
+
+# Initial value of i
 i = 0
+# Loop starts
 while i != answer:
-    i = float(input())
-    if i == 0:
+    i = float(input())      # enter value of i here
+
+    if i == 0:  # break the loop by pressing 0 and it will exit you out of the game
         print("Bye Bye !!!")
         break
-    elif i == answer:
+    elif i == answer: # Guess the right answer to break the loop
         print("You guessed it. It was {}".format(answer))
         break
-    else:
+    else: # if the above conditions don't satisfy then the loop keeps running until you press 0 and exit or guess the right answer
         if i < answer:
             print("Please guess higher")
         else:
